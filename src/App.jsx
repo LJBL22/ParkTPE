fd12cab995d67727d799881ae090264f90020f86
LJBL22 <yourgitemail@example.com>
2023-04-14 18:22:17 +0800

    feat: add a dummy Popup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,44 @@
 import './App.css';
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { parkingLot, available } from './dummyData';
+import { tw97ToWGS84 } from './utility';
 
 function App() {
-  // first get the viewport height and multiple it by 1% to get a value for a vh unit
+  // 處理 safari 問題 first get the viewport height and multiple it by 1% to get a value for a vh unit
   let vh = window.innerHeight * 0.01;
   // then set the value in the --vh custom property to the root of the document
   document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+  // 預設的中心位置
+  // const defaultPosition = [25.044761, 121.536651];
+  const defaultPosition = [25.037146691182055, 121.56628039692905];
+
+  // 先取一個 data 座標試試看
+  // const getPosition = () => {
+  // const x = parkingLot.data.park[1].tw97x;
+  // const y = parkingLot.data.park[1].tw97y;
+  const { id, name, payex, tw97x: x, tw97y: y } = parkingLot.data.park[1];
+  const WGS84 = tw97ToWGS84(x, y);
+  const { lat, lng } = WGS84;
+  const markerPosition = [lat, lng];
+  console.log(markerPosition);
+  //   return newPosition;
+  // };
+
+  // const newPosition = getPosition();
+  // console.log(newPosition);
+
+  // 嘗試 render dummy markers
+  // const renderMarkers = markers.map((marker)=> {
+  //   return (
+  //     <Marker key={} position={position}>
+  //       <Popup>
+  //         A pretty CSS3 popup. <br /> Easily customizable.
+  //       </Popup>
+  //     </Marker>
+  //   );
+  // })
+
   return (
     <>
       <nav className='nav-top'>
@@ -13,7 +46,9 @@ function App() {
       </nav>
       <div id='map'>
         <MapContainer
-          center={[25.0410601, 121.5428832]}
+          center={defaultPosition}
+          // 要搭配 bound 來安排一下
+          // minZoom={10}
           zoom={15}
           scrollWheelZoom={true}
         >
@@ -21,6 +56,17 @@ function App() {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
             url='https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png'
           />
+          {/* 設定使用者一進入畫面的位置 */}
+
+          {/* 嘗試 抓取一個 dummy marker */}
+          <Marker position={markerPosition}>
+            <Popup>
+              <h3>{name}</h3>
+              <p>{payex}</p>
+            </Popup>
+          </Marker>
+          {/* 嘗試 render dummy marker */}
+          {/* {renderMarkers} */}
         </MapContainer>
       </div>
     </>
---
+++ src/dummyData.js
@@ -0,0 +1,257 @@
+export const parkingLot = {
+  "data": {
+    "UPDATETIME": "Tue Apr 11 00:00:00 CST 2023",
+    "park": [
+      {
+        "id": "001",
+        "area": "信義區",
+        "name": "府前廣場地下停車場",
+        "type": "1",
+        "type2": "2",
+        "summary": "為地下二層停車場，計有1998個小型車停車格，1337個機車停車位",
+        "address": "松壽路1號地下",
+        "tel": "2722-0152",
+        "payex": "配合2/1-2/19台灣燈會期間費率調整為，周一至周五17-22時，週六至周日與行政機關放假之紀念日與民俗日10時至22時，調整小型車費率，計時60元，其餘時段維持原費率。小型車全日月票4200元，夜間月票1000元(限周一至周五19-8，及周六、日與行政機關放假之紀念日、民俗日)，小型車計時30元(9-18)，夜間計時10元(18-9)；機車計時10元(當日當次上限20元)，機車月票300元。",
+        "serviceTime": "00:00:00~23:59:59",
+        "tw97x": "306812.928",
+        "tw97y": "2769892.95",
+        "totalcar": 2043,
+        "totalmotor": 1360,
+        "totalbike": 0,
+        "totalbus": 0,
+        "Pregnancy_First": "40",
+        "Handicap_First": "45",
+        "Taxi_OneHR_Free": "0",
+        "AED_Equipment": "0",
+        "CellSignal_Enhancement": "0",
+        "Accessibility_Elevator": "0",
+        "Phone_Charge": "0",
+        "Child_Pickup_Area": "0",
+        "FareInfo": {
+          "WorkingDay": [
+            {
+              "Period": "00~09",
+              "Fare": "10"
+            },
+            {
+              "Period": "09~18",
+              "Fare": "30"
+            },
+            {
+              "Period": "18~24",
+              "Fare": "10"
+            }
+          ],
+          "Holiday": [
+            {
+              "Period": "00~09",
+              "Fare": "10"
+            },
+            {
+              "Period": "09~18",
+              "Fare": "30"
+            },
+            {
+              "Period": "18~24",
+              "Fare": "10"
+            }
+          ]
+        },
+        "EntranceCoord": {
+          "EntrancecoordInfo": [
+            {
+              "Xcod": "25.03648987",
+              "Ycod": "121.5621068",
+              "Address": "基隆路一段"
+            },
+            {
+              "Xcod": "25.036014",
+              "Ycod": "121.563163",
+              "Address": "松壽路"
+            },
+            {
+              "Xcod": "25.035975",
+              "Ycod": "121.561532",
+              "Address": "基隆路一段車行地下道"
+            }
+          ]
+        }
+      },
+      {
+        "id": "002",
+        "area": "信義區",
+        "name": "松壽廣場地下停車場",
+        "type": "1",
+        "type2": "2",
+        "summary": "立體式小型車446格(含身心障礙停車位6格)。",
+        "address": "松智路15號地下",
+        "tel": "27057716",
+        "payex": "配合2/1-2/19台灣燈會期間費率調整為，周一至周五17-22時，週六至周日與行政機關放假之紀念日與民俗日10時至22時，調整小型車費率，計時60元，其餘時段維持原費率。",
+        "serviceTime": "00:00:00~23:59:59",
+        "tw97x": "307143.740",
+        "tw97y": "2770011.52",
+        "totalcar": 455,
+        "totalmotor": 0,
+        "totalbike": 0,
+        "totalbus": 0,
+        "Pregnancy_First": "8",
+        "Handicap_First": "9",
+        "totallargemotor": "0",
+        "ChargingStation": "4",
+        "Taxi_OneHR_Free": "0",
+        "AED_Equipment": "0",
+        "CellSignal_Enhancement": "0",
+        "Accessibility_Elevator": "0",
+        "Phone_Charge": "0",
+        "Child_Pickup_Area": "0",
+        "FareInfo": {
+          "WorkingDay": [
+            {
+              "Period": "00~09",
+              "Fare": "10"
+            },
+            {
+              "Period": "09~22",
+              "Fare": "30"
+            },
+            {
+              "Period": "22~24",
+              "Fare": "10"
+            }
+          ],
+          "Holiday": [
+            {
+              "Period": "00~09",
+              "Fare": "10"
+            },
+            {
+              "Period": "09~22",
+              "Fare": "40"
+            },
+            {
+              "Period": "22~24",
+              "Fare": "10"
+            }
+          ]
+        },
+        "EntranceCoord": {
+          "EntrancecoordInfo": [
+            {
+              "Xcod": "25.036966",
+              "Ycod": "121.565523",
+              "Address": ""
+            }
+          ]
+        }
+      },
+      {
+        "id": "003",
+        "area": "信義區",
+        "name": "臺北市災害應變中心地下停車場",
+        "type": "1",
+        "type2": "1",
+        "summary": "立體式小型車169格(含身心障礙停車位5格)。立體式機車208格(含身心障礙停車位5格)。",
+        "address": "莊敬路391巷11弄2號地下",
+        "tel": "2345-3859",
+        "payex": "小型車計時30元，全日月票5000元，日間月票2600元(7-19) 機車計時10元(當日當次最高收費上限20元，隔日另計)；月票300元；本場實施差別費率，停放於電動車格之車輛，依該場停車費率每小時加收10元，未充電者每小時再加收10元(合計每小時加收20元)。",
+        "serviceTime": "00:00:00~23:59:59",
+        "tw97x": "307142.121",
+        "tw97y": "2769087.67",
+        "totalcar": 169,
+        "totalmotor": 200,
+        "totalbike": 0,
+        "totalbus": 0,
+        "Pregnancy_First": "3",
+        "Handicap_First": "5",
+        "totallargemotor": "1",
+        "ChargingStation": "2",
+        "Taxi_OneHR_Free": "0",
+        "AED_Equipment": "0",
+        "CellSignal_Enhancement": "0",
+        "Accessibility_Elevator": "0",
+        "Phone_Charge": "0",
+        "Child_Pickup_Area": "0",
+        "FareInfo": {
+          "WorkingDay": [
+            {
+              "Period": "00~24",
+              "Fare": "30"
+            }
+          ],
+          "Holiday": [
+            {
+              "Period": "00~24",
+              "Fare": "30"
+            }
+          ]
+        },
+        "EntranceCoord": {
+          "EntrancecoordInfo": [
+            {
+              "Xcod": "25.028728",
+              "Ycod": "121.566111",
+              "Address": ""
+            }
+          ]
+        }
+      },
+      {
+        "id": "10014",
+        "area": "北投區",
+        "name": "Uspace車位共享平台停車場(北投區)",
+        "type": "2",
+        "summary": "建物附設立體式小型車1格。",
+        "address": "",
+        "tel": "5568-6554",
+        "payex": "計時：60元/時，停車未滿1小時者，以1小時收費，停車逾1小時以上，其超過之不滿1小時部分，如未逾半小時者以半小時計算。",
+        "serviceTime": "每日不逾8小時，每月不逾240小時。",
+        "tw97x": "302109.61098",
+        "tw97y": "2777665.06189",
+        "totalcar": 1,
+        "totalmotor": 0,
+        "totalbike": 0,
+        "totalbus": 0,
+        "FareInfo": {},
+        "EntranceCoord": {}
+      }
+    ]
+  }
+}
+
+export const available = {
+  "data": {
+    "UPDATETIME": "Tue Apr 11 15:31:00 CST 2023",
+    "park": [
+      {
+        "id": "001",
+        "availablecar": 247,
+        "availablemotor": 63,
+        "availablebus": -9
+      },
+      {
+        "id": "002",
+        "availablecar": 0,
+        "availablemotor": -9,
+        "availablebus": -9
+      },
+      {
+        "id": "003",
+        "availablecar": 37,
+        "availablemotor": 61,
+        "availablebus": 0,
+        "ChargeStation": {
+          "scoketStatusList": [
+            {
+              "spot_abrv": "177",
+              "spot_status": "待機中"
+            },
+            {
+              "spot_abrv": "176",
+              "spot_status": "充電中"
+            }
+          ]
+        }
+      }
+    ]
+  }
+}
