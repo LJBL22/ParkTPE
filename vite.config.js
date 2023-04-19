1187bd48f6a8fa712675a15f09f8460f171638af
LJBL22 <ljbl2208@gmail.com>
2023-04-19 19:38:19 +0800

    feat: github page deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // 本地端不加
+  base: process.env.NODE_ENV === 'production' ? '/ParkTPE/' : '/',
   plugins: [react()],
   server: {
     port: 5024,
